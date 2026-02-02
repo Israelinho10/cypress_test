90a1d38558279a06baf1a3900ab1b12a63370827
israelinho <chavez.israel25@gmail.com>
2026-02-02 17:48:54 -0600

    Add examples and forms test js files

---
+++ cypress/e2e/examples.cy.js
@@ -0,0 +1,80 @@
+describe('Variopus examples', ()=>{
+    beforeEach(()=>{
+        cy.visit('/examples')
+    })
+
+    it('Multi-page testing', ()=>{
+        cy.getDataTest('nav-why-cypress').click();
+        cy.location("pathname").should('equal', '/')
+
+        cy.getDataTest('nav-overview').click();
+        cy.location("pathname").should('equal', '/overview')
+
+        cy.getDataTest('nav-fundamentals').click();
+        cy.location("pathname").should('equal', '/fundamentals')
+    })
+
+    it.skip('Intercepts body', ()=>{
+        cy.intercept("POST", "http://localhost:3000/examples", {
+            body: {
+                message: 'successfully intercepted request'
+            }
+        })
+        cy.getDataTest(('post-button')).click()
+    })
+
+    it.skip('Intercepts', ()=>{
+        cy.intercept("POST", "http://localhost:3000/examples", {
+            fixture: 'example.json'
+        })
+        cy.getDataTest(('post-button')).click()
+    })
+
+    it.only('Grudge list', ()=>{
+        cy.contains(/add some grudges/i)
+        cy.getDataTest('grudge-list').within(()=>{
+            cy.get('li').should('have.length', 0)
+        })
+
+        cy.getDataTest('clear-button').should('not.exist')
+
+        cy.getDataTest('grudge-list-title').should('have.text', 'Add Some Grudges')
+
+        cy.getDataTest('grudge-input').within(()=>{
+            cy.get('input').type('some grudge')
+        })
+        cy.getDataTest('add-grudge-button').click()
+        cy.getDataTest('grudge-list').within(()=>{
+            cy.get('li').should('have.length', 1)
+        })
+
+        cy.getDataTest('grudge-list-title').should('have.text', 'Grudges')
+
+        cy.getDataTest('grudge-input').within(()=>{
+            cy.get('input').type('number 2')
+        })
+        cy.getDataTest('add-grudge-button').click()
+        cy.getDataTest('grudge-list').within(()=>{
+            cy.get('li').should('have.length', 2)
+            cy.get('li').its(0).should('contain.text', 'some grudge');
+        })
+
+        cy.getDataTest('grudge-list').within(()=>{
+            cy.get('li').its(0).within(()=>{
+                cy.get('button').click()
+            })
+        })
+
+        cy.getDataTest('grudge-list').within(()=>{
+            cy.get('li').should('have.length', 1)
+        })
+
+        cy.getDataTest('clear-button').click()
+
+        cy.getDataTest('grudge-list').within(()=>{
+            cy.get('li').should('have.length', 0)
+        })
+
+        cy.getDataTest('grudge-list-title').should('have.text', 'Add Some Grudges')
+    })
+})
---
+++ cypress/e2e/forms.cy.js
@@ -0,0 +1,26 @@
+describe('Form tests', () => {
+    beforeEach(()=>{
+        cy.visit('/forms')
+    })
+
+    it('Test subscribe form', ()=>{
+        cy.contains(/Testing forms/i)
+        cy.getDataTest('subscribe-form').find('input').as('subscribe-input')
+        cy.get('@subscribe-input').type('ryan@coderyan.com')
+        cy.contains(/Successfully subbed: ryan@coderyan.com!/i).should('not.exist')
+        cy.getDataTest('susbscribe-button').click()
+        cy.contains(/Successfully subbed: ryan@coderyan.com!/i).should('exist')
+        cy.wait(3000)
+        cy.contains(/Successfully subbed: ryan@coderyan.com!/i).should('not.exist')
+
+        cy.get('@subscribe-input').type('ryan@coderyan.io')
+        cy.contains(/Invalid email: ryan@coderyan.io!/i).should('not.exist')
+        cy.getDataTest('susbscribe-button').click()
+        cy.contains(/Invalid email: ryan@coderyan.io!/i).should('exist')
+
+        cy.wait(3000)
+        cy.contains(/fail!/i).should('not.exist')
+        cy.getDataTest('susbscribe-button').click()
+        cy.contains(/fail!/i).should('exist')
+    })
+})
